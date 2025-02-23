57e286b8df46e2cb05df03a40db51940699a5a49
charlieroth <charlieroth4@gmail.com>
2025-02-23 22:18:22 +0100

    Update order of columns in employees table

--- migrations/1740337501_add_tables.up.sql
+++ migrations/1740337501_add_tables.up.sql
@@ -4,12 +4,10 @@ CREATE TABLE IF NOT EXISTS us_states (
     state_abbr VARCHAR(2),
     state_region VARCHAR(50)
 );
-
 CREATE TABLE IF NOT EXISTS region (
     region_id SMALLINT NOT NULL PRIMARY KEY,
     region_description BPCHAR NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS suppliers (
     supplier_id SMALLINT NOT NULL PRIMARY KEY,
     company_name VARCHAR(40) NOT NULL,
@@ -24,11 +22,10 @@ CREATE TABLE IF NOT EXISTS suppliers (
     fax VARCHAR(24),
     homepage TEXT
 );
-
 CREATE TABLE IF NOT EXISTS employees (
     employee_id SMALLINT NOT NULL PRIMARY KEY,
-    first_name VARCHAR(10) NOT NULL,
     last_name VARCHAR(20) NOT NULL,
+    first_name VARCHAR(10) NOT NULL,
     title VARCHAR(30),
     title_of_courtesy VARCHAR(25),
     birth_date DATE,
@@ -46,13 +43,11 @@ CREATE TABLE IF NOT EXISTS employees (
     photo_path VARCHAR(255),
     FOREIGN KEY (reports_to) REFERENCES employees
 );
-
 CREATE TABLE IF NOT EXISTS shippers (
     shipper_id SMALLINT NOT NULL PRIMARY KEY,
     company_name VARCHAR(40) NOT NULL,
     phone VARCHAR(24)
 );
-
 CREATE TABLE IF NOT EXISTS customers (
     customer_id BPCHAR NOT NULL PRIMARY KEY,
     company_name VARCHAR(40) NOT NULL,
@@ -66,19 +61,16 @@ CREATE TABLE IF NOT EXISTS customers (
     phone VARCHAR(24),
     fax VARCHAR(24)
 );
-
 CREATE TABLE IF NOT EXISTS categories (
     category_id SMALLINT NOT NULL PRIMARY KEY,
     category_name VARCHAR(15) NOT NULL,
     description TEXT,
     picture BYTEA
 );
-
 CREATE TABLE IF NOT EXISTS customer_demographics (
     customer_type_id BPCHAR NOT NULL PRIMARY KEY,
     customer_desc TEXT
 );
-
 CREATE TABLE IF NOT EXISTS customer_customer_demo (
     customer_id BPCHAR NOT NULL,
     customer_type_id BPCHAR NOT NULL,
@@ -86,14 +78,12 @@ CREATE TABLE IF NOT EXISTS customer_customer_demo (
     FOREIGN KEY (customer_id) REFERENCES customers,
     FOREIGN KEY (customer_type_id) REFERENCES customer_demographics
 );
-
 CREATE TABLE IF NOT EXISTS territories (
     territory_id VARCHAR(20) NOT NULL PRIMARY KEY,
     territory_description BPCHAR NOT NULL,
     region_id SMALLINT NOT NULL,
     FOREIGN KEY (region_id) REFERENCES region
 );
-
 CREATE TABLE IF NOT EXISTS employee_territories (
     employee_id SMALLINT NOT NULL,
     territory_id VARCHAR(20) NOT NULL,
@@ -101,7 +91,6 @@ CREATE TABLE IF NOT EXISTS employee_territories (
     FOREIGN KEY (employee_id) REFERENCES employees,
     FOREIGN KEY (territory_id) REFERENCES territories
 );
-
 CREATE TABLE IF NOT EXISTS products (
     product_id SMALLINT NOT NULL PRIMARY KEY,
     product_name VARCHAR(40) NOT NULL,
@@ -116,7 +105,6 @@ CREATE TABLE IF NOT EXISTS products (
     FOREIGN KEY (supplier_id) REFERENCES suppliers,
     FOREIGN KEY (category_id) REFERENCES categories
 );
-
 CREATE TABLE IF NOT EXISTS orders (
     order_id SMALLINT NOT NULL PRIMARY KEY,
     customer_id BPCHAR,
@@ -136,7 +124,6 @@ CREATE TABLE IF NOT EXISTS orders (
     FOREIGN KEY (employee_id) REFERENCES employees,
     FOREIGN KEY (ship_via) REFERENCES shippers
 );
-
 CREATE TABLE IF NOT EXISTS order_details (
     order_id SMALLINT NOT NULL,
     product_id SMALLINT NOT NULL,
